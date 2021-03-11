3b3c71858c31cd4cbbdc47010a09f56cb40bd1a8
Burcu VARICI <burcuvarici3535@gmail.com>
2021-03-11 23:02:56 +0300

    inbox güncel

--- inbox.js
+++ inbox.js
@@ -1,8 +1,28 @@
 function tumunuSec() {
 
-
+    var checkbox = document.getElementsByClassName("checkbox");
     for (var i = 0; i < checkbox.length; i++) {
-        checkbox.elements[i].checked = checked;
+        checkbox[i].checked = true;
+
     }
 
 }
+
+function burcu(event) {
+    var content = document.getElementsByName("b");
+    var checkbox = document.getElementsByClassName("checkbox");
+    for (var i = 0; i < content.length; i++) {
+        content[i].className.replace(" active", "");
+    }
+
+    for (var i = 0; i < content.length; i++) {
+        if (checkbox[i].checked) {
+            content[i].style.display = "none";
+        }
+        else {
+            content[i].style.display = "grid";
+        }
+    }
+
+}
+
